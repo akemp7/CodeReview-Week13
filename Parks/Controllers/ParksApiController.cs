14a73eba51b9de70dfa4d81bc78066d179a55d82
Anita Kemp <anita.k.kemp@gmail.com>
2019-11-01 10:05:11 -0700

    Add initial migration and get/post routes in controller

--- Parks/Controllers/ParksApiController.cs
+++ Parks/Controllers/ParksApiController.cs
@@ -18,7 +18,37 @@ namespace Parks.Controllers
             _db = db;
         }
 
+        // GET api/parks
         [HttpGet]
-        public ActionResult <IEnumerable<Park>> Get()
+        public ActionResult <IEnumerable<Park>> Get(int parkId, string name, string land, string state)
+        {
+            var query = _db.Parks.AsQueryable();
+            if(parkId != 0)
+            {
+                query = query.Where(p => p.ParkId == parkId);
+            }   
+            if(name != null)
+            {
+                query = query.Where(n => n.Name == name);
+            }
+            if(land != null)
+            {
+                query = query.Where(l => l.Land == land);
+            }
+            if(state != null)
+            {
+                query = query.Where(s => s.State == state);
+            }
+
+            return query.ToList();
+        }
+
+        // POST api/parks
+        [HttpPost]
+         public void Post([FromBody] Park parks)
+         {
+            _db.Parks.Add(parks);
+            _db.SaveChanges();
+         }
     }
 }

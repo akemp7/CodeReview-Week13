6bd7ca43bc33840967d71443afa9259930c53e49
Anita Kemp <anita.k.kemp@gmail.com>
2019-11-01 13:12:29 -0700

    Edit erroneous code parks model in client directory

--- ParksClient/Models/Parks.cs
+++ ParksClient/Models/Parks.cs
@@ -7,8 +7,8 @@ namespace ParksClient.Models {
     public class Park {
         public int ParkId { get; set; }
         public string Name { get; set; }
-        public double Land { get; set; }
-        public int State { get; set; }
+        public string Land { get; set; }
+        public string State { get; set; }
 
         public static List<Park> GetParks() 
         {
@@ -16,10 +16,10 @@ namespace ParksClient.Models {
             var result = apiCallTask.Result;
 
             JArray jsonResponse = JsonConvert.DeserializeObject<JArray> (result);
-           List<Park> parkList = new List<Park> ();
-            foreach (JObject jo in jsonResponse) {
-                Park newPark = JsonConvert.DeserializeObject<Park> (jo.ToString ());
-                parkList.Add (newPark);
+            List<Park> parkList = new List<Park> ();
+                foreach (JObject jo in jsonResponse) {
+                    Park newPark = JsonConvert.DeserializeObject<Park> (jo.ToString ());
+                    parkList.Add (newPark);
             }
 
             return parkList;
